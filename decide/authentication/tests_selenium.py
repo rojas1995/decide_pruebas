dbf187e337d903d520a4d730cba2cfb71b694b06
jagalindo <jagalindo@us.es>
2019-11-15 23:42:07 +0100

    Changing the user for the selenium test to practica

--- decide/authentication/tests_selenium.py
+++ decide/authentication/tests_selenium.py
@@ -1,22 +1,15 @@
 import unittest
 from selenium import webdriver
-from django.contrib.auth.models import User
 
 class TestSignup(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
         
-        u2 = User(username='admin')
-        u2.set_password('admin')
-        u2.is_superuser = True
-        u2.save()
-
-
     def test_signup_fire(self):
         self.driver.get("http://localhost:5000/admin/login/?next=/admin/")
-        self.driver.find_element_by_id('id_username').send_keys("admin")
-        self.driver.find_element_by_id('id_password').send_keys("admin")
+        self.driver.find_element_by_id('id_username').send_keys("practica")
+        self.driver.find_element_by_id('id_password').send_keys("practica")
         self.driver.find_element_by_id('login-form').click()
         self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))>0) 
     def tearDown(self):
