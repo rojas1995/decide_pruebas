371595b6a073384475e8f93822ea6edb103b6c6f
José Manuel Rojas Granado <jrojasgranado@gmail.com>
2019-11-25 18:37:09 +0100

    cabio la /

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'https://prueba-devide.herokuapp.com/'
+BASEURL = 'https://prueba-devide.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
